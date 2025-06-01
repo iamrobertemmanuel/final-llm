e3aaf1bd5e7c1f31d376482d89b7c341357d7820
iamrobertemmanuel <116332895+iamrobertemmanuel@users.noreply.github.com>
2025-06-01 23:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Local Multimodal AI Chat - Multimodal chat application with Gemini
-# Copyright (C) 2024 Leon Sander
+# 
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
